1101d71dda3c208a7534afa47270d41921fb55e2
NikkitaAnnaGeroge <nikkitaannageorge2028@cs.sjcetpalai.ac.in>
2025-07-19 22:09:47 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -15,9 +15,9 @@ String name;
 int rollNumber;
 char grade;
 public void displayDetails(){
-          System.out.println("name:" +name);
-          System.out.println("rollNumber:" +rollNumber);
-          System.out.println("grade:" +grade);
+          System.out.println("Name:" +name);
+          System.out.println("Roll Number:" +rollNumber);
+          System.out.println("Grade:" +grade);
      
              
     }
